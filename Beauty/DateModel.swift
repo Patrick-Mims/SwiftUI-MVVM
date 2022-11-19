3614e0c26fbfbf4508aca4f1c5083db900f50f85
Patrick Mims <patrickkevinmims@gmail.com>
2022-11-19 10:45:43 -0800

    Latest

--- Beauty/DateModel.swift
+++ Beauty/DateModel.swift
@@ -1,13 +1,8 @@
-/*
-    DateModel
- */
+//
+//  DateModel.swift
+//  Beauty
+//
+//  Created by JPL-ST-SPRING2022 on 11/19/22.
+//
 
 import Foundation
-
-struct DateModel: Identifiable {
-    let id = UUID
-    var age: Int
-    var img: String
-    var name: String
-    var profile: String
-}
--- Beauty/DateViewModel.swift
+++ Beauty/DateViewModel.swift
@@ -3,29 +3,23 @@ import Foundation
 
 class DateViewModel: ObservableObject {
     @Published var dateData: [DateModel]
-    @Published var orderData: [OrderModel]
     init() {
         dateData = [
+            DateModel(age: "25", name: "Anne", photo: "anne", profile: "Today is my birthday, with me Hapy Birthday!"),
             DateModel(
-                age: "25",
-                name: "Anne",
-                photo: "anne",
-                profile: "Today is my birthday, with me Hapy Birthday!"
-            ),
-            DateModel(
-                age: "35",
+                age: 35,
                 name: "Sarah",
                 photo: "sarah",
                 profile: "Yesterday, I went to the mall."
             ),
             DateModel(
-                age: "45",
+                age: 45,
                 name: "Kim",
                 photo: "kim",
                 profile: "My only clean shirt is in the laundry"
             ),
             DateModel(
-                age: "55",
+                age: 55,
                 name: "Jessica",
                 photo: "jessica",
                 profile: "Why can't men just get to know me?"
