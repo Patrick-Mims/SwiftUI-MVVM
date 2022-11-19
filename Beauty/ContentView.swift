b163630b610a9c31b76784b304eaef1d009e6ef3
Patrick Mims <patrickkevinmims@gmail.com>
2022-11-18 19:16:15 -0800

    Initial Commit

---
+++ Beauty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beauty
+//
+//  Created by JPL-ST-SPRING2022 on 11/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
