eccd45f5f9cf3b12c80ef48e35c2dc9da566c2f4
Patrick Mims <patrickkevinmims@gmail.com>
2022-11-18 21:12:07 -0800

    Latest

--- Beauty/ContentView.swift
+++ Beauty/ContentView.swift
@@ -1,11 +1,12 @@
 /*
-    Date: 11.18.22
-    Name: Beauty
-    Purpose: A Dating App
+ Date: 11.18.22
+ Name: Beauty
+ Purpose: Dating
  */
 import SwiftUI
 
 struct ContentView: View {
+    
     @EnvironmentObject var appData: DateViewModel
     
     var body: some View {
